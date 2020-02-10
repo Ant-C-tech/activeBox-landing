0d4db42dc6c7e3cbabbb8b61b760437f1b4b311e
Ant-C-tech <chivikovanton@gmail.com>
2020-02-10 17:22:08 +0200

    services and works section

---
+++ js/gallery.js
@@ -0,0 +1,16 @@
+var $links = $('.flexContainer').find('> a'),
+    $lightbox = $('.lightbox'),
+    $overlay = $('.overlay'),
+    targetImg;
+
+$links.click(function (event) {
+    event.preventDefault();
+    targetImg = $(this).attr('href');
+    $lightbox.find('img').attr('src', targetImg);
+    $lightbox.fadeIn();
+
+});
+
+$overlay.click(function () {
+    $lightbox.fadeOut();
+});
